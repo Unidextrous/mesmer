5e6ca041643582f00dadd274126f637411dfa0dc
Unidextrous <99895694+Unidextrous@users.noreply.github.com>
2026-07-26 00:50:23 -0400

    Implement basic ModernGL shader renderer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-
+import moderngl
+import numpy as np
 
 def main():
     pygame.init()
@@ -9,13 +10,47 @@ def main():
         pygame.OPENGL | pygame.DOUBLEBUF | pygame.FULLSCREEN
     )
 
+    ctx = moderngl.create_context()
+
+    with open("shaders/vertex.vert") as f:
+        vertex_shader = f.read()
+
+    with open("shaders/fragment.frag") as f:
+        fragment_shader = f.read()
+
+    program = ctx.program(
+        vertex_shader=vertex_shader,
+        fragment_shader=fragment_shader
+    )
+
+    vertices = np.array([
+        -1.0, -1.0,
+        1.0, -1.0,
+        -1.0, 1.0,
+
+        -1.0, 1.0,
+        1.0, -1.0,
+        1.0, 1.0
+    ], dtype='f4')
+
+    vbo = ctx.buffer(vertices.tobytes())
+
+    vao = ctx.vertex_array(
+        program,
+        [
+            (vbo, '2f', 'in_position')
+        ],
+    )
+
     running = True
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
+    
+        vao.render()
+        
         pygame.display.flip()
 
     pygame.quit()
