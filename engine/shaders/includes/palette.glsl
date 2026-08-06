58c343603cc94fbd72ea2d0ce7b6448e7c7d3755
Unidextrous <99895694+Unidextrous@users.noreply.github.com>
2026-08-06 01:06:24 -0400

    Expand palette.glsl to support color arrays
    Note: palette system is not functional yet, shaders may be broken for this commit

--- engine/shaders/includes/palette.glsl
+++ engine/shaders/includes/palette.glsl
@@ -1,13 +1,16 @@
 //------------------------------------------------------------
+// Palette Constants
+//------------------------------------------------------------
+
+const int MAX_PALETTE_SIZE = 8;
+
+
+//------------------------------------------------------------
 // Palette Uniforms
 //------------------------------------------------------------
 
 uniform int u_palette_size;
-
-uniform vec3 u_palette_0;
-uniform vec3 u_palette_1;
-uniform vec3 u_palette_2;
-uniform vec3 u_palette_3;
+uniform vec3 u_palette[MAX_PALETTE_SIZE];
 
 //------------------------------------------------------------
 // Palette Sampling
@@ -19,58 +22,26 @@ vec3 sample_palette(float t)
 
     if (u_palette_size <= 1)
     {
-        return u_palette_0;
+        return u_palette[0];
     }
 
-    if (u_palette_size == 2)
-    {
-        return mix(
-            u_palette_0,
-            u_palette_1,
-            t
-        );
-    }
-
-    if (u_palette_size == 3)
-    {
-        if (t < 0.5)
-        {
-            return mix(
-                u_palette_0,
-                u_palette_1,
-                t * 2.0
-            );
-        }
-
-        return mix(
-            u_palette_1,
-            u_palette_2,
-            (t - 0.5) * 2.0
-        );
-    }
-
-
-    if (t < 1.0 / 3.0)
-    {
-        return mix(
-            u_palette_0,
-            u_palette_1,
-            t * 3.0
-        );
-    }
-
-    if (t < 2.0 / 3.0)
-    {
-        return mix(
-            u_palette_1,
-            u_palette_2,
-            (t - 1.0 / 3.0) * 3.0
-        );
-    }
+    float position =
+        t * float(u_palette_size - 1);
+    
+    int index =
+        int(floor(position));
+    
+    index = min(
+        index,
+        u_palette_size - 2
+    );
+    
+    float fraction =
+        fract(position);
 
     return mix(
-        u_palette_2,
-        u_palette_3,
-        (t - 2.0 / 3.0) * 3.0
+        u_palette[index],
+        u_palette[index + 1],
+        fraction
     );
 }
